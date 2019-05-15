113437febf17f453098d270c32e7dc2fbdca41eb
SHEY LOUIS CHIA <shloch2007@yahoo.fr>
2019-05-15 11:29:35 +0100

    testing comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :post
+
+      validates :text,  presence: true, length: { minimum: 6 }
+      validates :user_id,  presence: true
+      validates :post_id,  presence: true
 end
