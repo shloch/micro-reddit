4949c45a5890d8c735e8cdf470c61d4bf569addb
pwilson77 <p.o.yirenkyi@gmail.com>
2019-05-15 09:48:22 +0000

    Playing with Associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,13 @@
 class Post < ApplicationRecord
+    belongs_to  :user
+    has_many  :comments
+
     validates :title,  presence: true, length: { minimum: 10 }
     validates :category,  length: { minimum: 3 }
     VALID_URL_REGEX = /(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/i 
       validates :url, presence: true, length: { maximum: 255 },
                     format: { with: VALID_URL_REGEX },
                     uniqueness: { case_sensitive: false }
+
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_155055) do
+ActiveRecord::Schema.define(version: 2019_05_15_093917) do
 
   create_table "comments", force: :cascade do |t|
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -24,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_05_14_155055) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
